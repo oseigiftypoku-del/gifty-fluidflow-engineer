dfdee9450e4c370bc6048835b6ae94b6ffd85de3
oseigiftypoku-del <oseigiftypoku@gmail.com>
2026-08-19 10:11:05 +0000

    Fix CSS syntax in Streamlit app

--- app.py
+++ app.py
@@ -91,8 +91,8 @@ st.markdown("""
         font-weight: 600;
     }
     </style>
-    """,unsafe_allow_html=True,
-)
+    """,unsafe_allow_html=True
+    )
 
 # -----------------------------------------------------------------------------
 # 2. MAIN HEADER & GUIDE
